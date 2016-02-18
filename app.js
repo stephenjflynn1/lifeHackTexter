baf2ee4e4f25f0d32cc4d53707fa8bee62aae94f
STEPHEN FLYNN <stephenflynn@STEPHENs-Air.home>
2016-02-18 13:51:29 -0500

    last commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var twilio = require('twilio');
+var http = require('http');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -22,9 +24,44 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Load the twilio module
+
+// Create a new REST API client to make authenticated requests against the
+// twilio back end
+var client = new twilio.RestClient('AC1b2ecbb48b05f6242114526bb723fd7a ', '4c1cf79973a8ed3105ea67dff0d38e95');
+
+// Pass in parameters to the REST API using an object literal notation. The
+// REST client will handle authentication and response serialzation for you.
+
 app.use('/', routes);
 app.use('/users', users);
+app.post('/sendmessage', function(req, res) {
+  client.sms.messages.create({
+      to: req.body.numberToText,
+      from:'+19177467597',
+      body: req.body.messageContent
+  }, function(error, message) {
+    console.log(message, 'response from twillo');
+      // The HTTP request to Twilio will run asynchronously. This callback
+      // function will be called when a response is received from Twilio
+      // The "error" variable will contain error information, if any.
+      // If the request was successful, this value will be "falsy"
+      if (!error) {
+          // The second argument to the callback will contain the information
+          // sent back by Twilio for the request. In this case, it is the
+          // information about the text messsage you just sent:
+          console.log('Success! The SID for this SMS message is:');
+          console.log(message.sid);
 
+          console.log('Message sent on:');
+          console.log(message.dateCreated);
+          res.json(message)
+      } else {
+          console.log(error);
+          res.json(error);
+      }
+    })
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- public/javascripts/textMessenger.js
+++ public/javascripts/textMessenger.js
@@ -1,44 +1,65 @@
+'use strict';
 
-var numberToText = document.getElementById("recipientNumber");
-var messageContent = document.getElementById("messageContent");
-
-
-
-// Load the twilio module
-var twilio = require('twilio');
-var http = require('http');
-
-// Create a new REST API client to make authenticated requests against the
-// twilio back end
-var client = new twilio.RestClient('AC1b2ecbb48b05f6242114526bb723fd7a ', '4c1cf79973a8ed3105ea67dff0d38e95');
-
-// Pass in parameters to the REST API using an object literal notation. The
-// REST client will handle authentication and response serialzation for you.
-
-
-document.getElementById("textInfo").onsubmit = function onSubmit(form)
-{
-client.sms.messages.create({
-    to: numberToText,
-    from:'+19177467597',
-    body: messageContent
-}, function(error, message) {
-    // The HTTP request to Twilio will run asynchronously. This callback
-    // function will be called when a response is received from Twilio
-    // The "error" variable will contain error information, if any.
-    // If the request was successful, this value will be "falsy"
-    if (!error) {
-        // The second argument to the callback will contain the information
-        // sent back by Twilio for the request. In this case, it is the
-        // information about the text messsage you just sent:
-        console.log('Success! The SID for this SMS message is:');
-        console.log(message.sid);
-
-        console.log('Message sent on:');
-        console.log(message.dateCreated);
-    } else {
-        console.log(error);
-    }
+var errorContainer = $('#errorContainer')
+var errorAlert = 'Please write 11 digit phone number in following format: 1##########'
+//validations
+$('#recipientNumber').keyup(function(event) {
+  errorContainer.text('')
+  var currentValue = $('#recipientNumber').val();
+  if (currentValue[0] != '1') {
+    errorContainer.text('Phone number should start with  \'1\'')
   }
+  if (/\D/.test(currentValue)) {
+    console.log('Input invalid: Input must only contain numbers');
+    return errorContainer.text(errorAlert)
+  }
+  if (currentValue.length > 11) {
+    return errorContainer.text('Number cannot be greater than 11 digits')
+  }
+});
+$("#recipientNumber").focusout(function() {
+  errorContainer.text('')
+  var currentValue = $('#recipientNumber').val();
+  if (currentValue[0] != '1') {
+    errorContainer.text('Phone number should start with  \'1\'')
+  }
+  if (/\D/.test(currentValue)) {
+    console.log('Input invalid: Input must only contain numbers');
+    return errorContainer.text(errorAlert)
+  }
+  if (currentValue.length > 11) {
+    return errorContainer.text('Number cannot be greater than 11 digits')
+  }
+});
+$('#textInfo').submit(function(event) {
+  //check if currentValue is less than 11
+  event.preventDefault();
+  var numberToText = $('#recipientNumber').val();
+  var messageContent = $('#messageContent').val()
+  if (numberToText.length < 11) {
+    return errorContainer.text('Number cannot be lesser than 11 digits')
+  }
+  var data = {
+    numberToText: numberToText,
+    messageContent: messageContent
+  };
+  var request = $.ajax({
+    type: "POST",
+    url: '/sendmessage',
+    data: data,
+    dataType: 'json'
+  })
+  request.done(function(data) {
+    console.log(data)
+    if (data.status != 200) {
+      errorContainer.text(data.message)
+    }
+  })
 
+  request.fail(function(jqXHR, status) {
+    console.log(jqXHR.message);
+    console.log(status.message);
+    errorContainer.text(jqXHR.message)
+    console.log('Request failed: ', status, jqXHR);
+  });
 });
