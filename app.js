3709f2acb712f89be5d3ccc5ca1d30158141c002
STEPHEN FLYNN <stephenjflynn1@gmail.com>
2016-06-15 21:03:59 -0400

    picture for title

--- app.js
+++ app.js
@@ -33,6 +33,20 @@ var client = new twilio.RestClient('AC1b2ecbb48b05f6242114526bb723fd7a ', '4c1cf
 // Pass in parameters to the REST API using an object literal notation. The
 // REST client will handle authentication and response serialzation for you.
 
+
+
+var lifeHacks[];
+lifeHacks[0] = "LIFE HACK: When showing up late for work, don't show up with a cup of coffee you bought along the way";
+lifeHacks[1] = "LIFE HACK: Trying to get work done? Try out video game music. It's designed to be background and not break your focus.";
+lifeHacks[2] = "LIFE HACK: Before checking in your luggage, take a picture of it. A picture is worth a thousand words if it gets lost."; 
+lifeHacks[3] = "LIFE HACK: When introducing people to each other, be sure to use their names several times after making the introduction.";
+lifeHacks[4] = "LIFE HACK: Use baby powder to easily remove sand after a day at the beach";
+lifeHacks[5] = "LIFE HACK: If you need professional photos of a small item, use your white bath as a backdrop.";
+lifeHacks[6] = "LIFE HACK: If someone gives you a gift, text the when you use it. It will make them feel good";
+lifeHacks[7] = "lIFE HACK: Before sending your device for a repair under warranty, record a video of the fault as proof.";
+lifeHacks[8] = "LIFE HACK: When working with contractors on a home project, record every step of the process.";
+lifeHacks[9] = "LIFE HACK: Make a running list of your professional accomplishments to have handy when negotiating for higher pay.";
+
 app.use('/', routes);
 app.use('/users', users);
 app.post('/sendmessage', function(req, res) {
@@ -85,6 +99,13 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
+
+
+http.createServer(app).listen(app.get('port'),
+  function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
+
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -92,6 +113,17 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+app.set('port', process.env.PORT || 8080);
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+app.listen(app.get('port'));
+
+
+
+
 
 
 module.exports = app;
